baedc05c5f5354a2f8814f8f64258908695bbd07
qhd <1756909433@qq.com>
2023-03-02 23:28:41 +0800

    Config客户端配置与测试

---
+++ cloud-config-client-3355/src/main/java/com/qhd/springcloud/server/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.qhd.springcloud.server;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: qhd
+ * @date: 2023-03-02
+ * @description:
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    //@Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
